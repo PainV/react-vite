48154a383fd86b4008fcbde0214ebeb6686f052e
Vincent <vincent.pain@epitech.eu>
2023-02-17 15:37:49 +0100

    Add router + New Page ShipDetails

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 import './App.css'
 import SimpleImage from './atoms/SimpleImage'
 import { IMAGE } from './constants'
@@ -14,10 +15,15 @@ type Ships = {
 
 const App = () => {
   const [ships, setShips] = useState<Ships[]>([])
+  const navigate = useNavigate()
 
   useEffect(() => {
     getShips().then(setShips)
   })
+
+  const goToDetailsPage = (shipId: string) => {
+    navigate(`details/${shipId}`)
+  }
 
   const renderHeader = (title: string, subtitle: string) => {
     return <CardHeader title={title} subtitle={subtitle} className='card-header' />
@@ -31,7 +37,7 @@ const App = () => {
   return (
     <div className="App">
       {ships.map((ship) => (
-        <div className="ship" key={ship.id}>
+        <div className="ship" key={ship.id} onClick={() => goToDetailsPage(ship.id)}>
           {renderHeader(ship.name, ship.type)}
           {renderPicture(ship.image)}
         </div>
